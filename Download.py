e27392df40fcc485bcba7582ccdd3c1e85935b03
Palafat <articfresh@mail.ru>
2019-06-17 11:13:00 +0300

    Download files

---
+++ Download.py
@@ -0,0 +1,12 @@
+# from selenium import webdriver
+# import os
+# browser = webdriver.Chrome(с)
+# link = "http://suninjuly.github.io/file_input.html"
+# browser.get(link)
+# browser.find_element_by_name('firstname').send_keys('Art')
+# browser.find_element_by_name('lastname').send_keys('Yer')
+# browser.find_element_by_name('email').send_keys("war@fdsf.rt")
+# current_dir = os.path.abspath(os.path.dirname("/home/user/programs/"))
+# file_path = os.path.join(current_dir, 'file.txt')
+# browser.find_element_by_name('file').send_keys(file_path)
+# browser.find_element_by_css_selector('[class="btn btn-primary"]').click()
