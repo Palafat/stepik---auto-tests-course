83d6060d26b7935b370ccea2718b6c1b0ee85d22
Palafat <articfresh@mail.ru>
2019-06-17 11:03:35 +0300

    Чат с Вадимом

---
+++ Vadim.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+import time
+link = "http://10.0.1.17"
+browser = webdriver.Chrome(executable_path='/home/user/Документы/Chromedriver/chromedriver')
+browser.get(link)
+time.sleep(2)
+browser.find_element_by_name("login").send_keys("375292361864")
+browser.find_element_by_name("password").send_keys("123456")
+browser.find_element_by_xpath("//button[contains(text(),'Войти')]").click()
+time.sleep(1)
+browser.find_element_by_xpath("//i[@class='fal fa-comments']").click()
+time.sleep(1)
+browser.find_element_by_xpath("//span[contains(text(),'Тестерович Тестер - Вадим Девелопер')]").click()
+browser.find_element_by_xpath("//textarea[@placeholder='Введите Сообщение']").send_keys("Прозноз погоды/Курсы")
+browser.find_element_by_xpath("//button[@class='chat-controls-submit btnSaveProfile']").click()
